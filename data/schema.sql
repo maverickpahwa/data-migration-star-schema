d035711e6d89ec56acafef38040be5100205b58b
maverickpahwa <123452891+maverickpahwa@users.noreply.github.com>
2023-01-24 02:41:13 -0700

    Create basic schema and tables
    Need to add DimOrders, DimOrderdetails, FactSales

---
+++ data/schema.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE sales_dm;
+\c sales_dm
+
+CREATE SCHEMA stage;
+CREATE SCHEMA dm;
+
+SET search_path = dm, pg_catalog;
+SET default_tablespace = '';
+SET default_with_oids = false;
+
+CREATE TABLE stage.LegacyOrders (
+    order_id TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    date_of_birth TEXT NOT NULL,
+    phone TEXT NULL,
+    email TEXT NOT NULL,
+    street TEXT NULL,
+    city TEXT NULL,
+    postal_code TEXT NULL,
+    state TEXT NULL,
+    order_date TEXT NOT NULL,
+    payment_type TEXT NOT NULL,
+    product_name TEXT NOT NULL,
+    price TEXT NOT NULL,
+    quantity TEXT NOT NULL,
+    order_total TEXT NOT NULL
+);
+
+CREATE TABLE DimCustomers(
+    customer_id TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    date_of_birth TEXT NOT NULL,
+    phone TEXT NULL,
+    email TEXT NOT NULL
+);
+
+create table DimAddressHistory(
+    customer_id TEXT NOT NULL,
+    street_number TEXT NULL,
+    city TEXT NULL,
+    postal_code TEXT NULL,
+    state TEXT NULL,
+    start_date TEXT NOT NULL,
+    end_date TEXT NOT NULL
+)
